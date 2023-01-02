beb290b5479f0df7c28a5f7195b6d51811fc44ce
sahilkochar10 <sahilkochar10@gmail.com>
2023-01-02 20:05:43 +0530

    shipper add and edit

--- flipkart/src/main/java/com/onlineshopping/flipkart/controller/ShipperController.java
+++ flipkart/src/main/java/com/onlineshopping/flipkart/controller/ShipperController.java
@@ -1,23 +1,41 @@
 package com.onlineshopping.flipkart.controller;
 
 import com.onlineshopping.flipkart.entity.Shippers;
+import com.onlineshopping.flipkart.exceptions.ShipperNotFoundException;
 import com.onlineshopping.flipkart.service.ShipperService;
+import com.onlineshopping.flipkart.util.ValidList;
+import com.onlineshopping.flipkart.vo.Shipper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/api/v1/shipper")
 public class ShipperController {
 
     @Autowired
     ShipperService shipperService;
 
-    @GetMapping("/api/v1/shipper")
+    @GetMapping
     ResponseEntity<List<Shippers>> getShippers(){
         return shipperService.getShippers();
+    }
+
+    @PostMapping
+    ResponseEntity<String> saveShippers(@RequestBody ValidList<Shipper> shippers) throws ShipperNotFoundException {
+        shipperService.saveShippers(shippers);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    ResponseEntity<String> updateShippers(@RequestBody @Valid List<com.onlineshopping.flipkart.vo.Shipper> shippers) throws ShipperNotFoundException {
+        shipperService.updateShippers(shippers);
+        return new ResponseEntity<>(HttpStatus.OK);
+
     }
 }
--- flipkart/src/main/java/com/onlineshopping/flipkart/service/ShipperService.java
+++ flipkart/src/main/java/com/onlineshopping/flipkart/service/ShipperService.java
@@ -1,11 +1,15 @@
 package com.onlineshopping.flipkart.service;
 
 import com.onlineshopping.flipkart.entity.Shippers;
+import com.onlineshopping.flipkart.exceptions.ShipperAlreadyExistsException;
+import com.onlineshopping.flipkart.exceptions.ShipperNotFoundException;
+import com.onlineshopping.flipkart.vo.Shipper;
 import org.springframework.http.ResponseEntity;
 import com.onlineshopping.flipkart.repository.ShipperRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,5 +20,36 @@ public class ShipperService {
     ShipperRepository shipperRepository;
     public ResponseEntity<List<Shippers>> getShippers() {
         return new ResponseEntity<>(shipperRepository.findAll(), HttpStatus.OK);
+    }
+
+    @Transactional(rollbackFor = {ShipperNotFoundException.class})
+    public void saveShippers(List<Shipper> shippers) throws ShipperNotFoundException {
+
+        for (Shipper shipper : shippers) {
+            if (shipperRepository.findById(shipper.getShipperID()).isPresent()) {
+                throw new ShipperAlreadyExistsException(shipper.getShipperID());
+            }
+            shipperRepository.save(getEntity(shipper));
+        }
+    }
+    // add transactionmanager
+    public com.onlineshopping.flipkart.entity.Shippers getShipperByShipid(Integer shipid) throws ShipperNotFoundException{
+        return shipperRepository.findById(shipid).orElseThrow(() -> new ShipperNotFoundException(shipid));
+    }
+
+    public void updateShippers(List<com.onlineshopping.flipkart.vo.Shipper> shippers) throws ShipperNotFoundException {
+        for (Shipper sh : shippers) {
+            if (shipperRepository.findById(sh.getShipperID()).isEmpty()) {
+                throw new ShipperNotFoundException(sh.getShipperID());
+            }
+            shipperRepository.save(getEntity(sh));
+        }
+    }
+
+    public Shippers getEntity(Shipper shippers) throws ShipperNotFoundException {
+        return Shippers.builder()
+                .shipperName(shippers.getShipperName())
+                .shipperID(shippers.getShipperID())
+                .phone(shippers.getPhone()).build();
     }
 }
